b47a48bd6450ccf9e10526f1e67d50c3b8a190d0
Kyle Fagnan <kfagnan19@gmail.com>
2021-11-02 15:36:45 -0600

    spinner refactored to spinner2

---
+++ spinner2.js
@@ -0,0 +1,59 @@
+const symbols = "|/-\\||/-\\|"
+let time = 0;
+
+
+for(const sym of symbols){
+  setTimeout(() => {
+    process.stdout.write(`\r${sym}     `);
+  }, time);
+  time += 200;
+}
+
+
+
+
+//Before refactoring
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 700);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\|   '); 
+// }, 900);
+
+// setTimeout(() => {
+//   process.stdout.write('\r|   ');
+// }, 1100);
+
+// setTimeout(() => {
+//   process.stdout.write('\r/   ');
+// }, 1300);
+
+// setTimeout(() => {
+//   process.stdout.write('\r-   ');
+// }, 1500);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\\   '); 
+// }, 1700);
+
+// setTimeout(() => {
+//   // Need to escape the backslash since it's a special character.
+//   process.stdout.write('\r\|   '); 
+// }, 1900);
